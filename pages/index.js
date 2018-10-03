37c2e6a28f018aa55c2f4f251d8576f9abe58f6a
igeligel <kevinigeligeligel@gmail.com>
2018-10-03 18:59:19 +0200

    feat: more general layout

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,51 @@ const Wrapper = styled.div`
   display: flex;
   min-height: 100vh;
   flex-direction: column;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
 `;
 
 const Content = styled.main`
   flex: 1;
+  max-width: 1000px;
+  min-width: 100%;
+  align-self: center;
+  box-sizing: border-box;
+
+  @media (min-width: 1000px) {
+    min-width: 1000px;
+  }
+`;
+
+const Header = styled.header`
+  max-width: 1000px;
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  min-width: 100%;
+
+  @media (min-width: 1000px) {
+    min-width: 1000px;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 export default () => (
   <Wrapper>
-    <header>…</header>
-    <Content>...dwa</Content>
+    <Header>
+      <div>interns</div>
+      <Menu>
+        <div>Companies</div>
+        <div>About</div>
+      </Menu>
+    </Header>
+    <Content>Our cool internships</Content>
     <footer>…</footer>
   </Wrapper>
 );
